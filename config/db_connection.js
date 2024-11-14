53b054633562f2b02201ae6eafdc7b2b344df2da
anasask2001 <anas.p8113@gmail.com>
2024-11-14 10:26:47 +0530

    "first commit"

--- config/db_connection.js
+++ config/db_connection.js
@@ -5,6 +5,9 @@ const Connect = () => {
   mongoose
     .connect(process.env.DB, {
       dbName: "users",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
     })
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log("MongoDB connection error:", err));
